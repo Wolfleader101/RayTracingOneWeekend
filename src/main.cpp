0f63896f2757fcaf40a203d6bda7c2cf788634aa
Wolfleader <james.morgan@iinet.net.au>
2023-04-07 15:00:37 +0800

    ray class

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,45 @@
 #include <iostream>
 
 #include "color.hpp"
+#include "ray.hpp"
 #include "vec3.hpp"
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float v = 0.5f * (unit_direction.y() + 1.0f);
+    return (1.0f - v) * color(1.0f, 1.0f, 1.0f) + v * color(0.5f, 0.7f, 1.0f);
+}
 
 int main() {
     // Image
-
-    const int image_width = 256;
-    const int image_height = 256;
+    const float aspect_ratio = 16.0f / 9.0f;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     std::ofstream file("images/image.ppm");
 
-    // Render
+    // Camera
+    float viewport_height = 2.0f;
+    float viewport_width = aspect_ratio * viewport_height;
+    float focal_length = 1.0f;
 
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // Render
     file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(float(i) / (image_height - 1), float(j) / (image_width - 1), 0.25);
+            auto u = float(i) / (image_width - 1);
+            auto v = float(j) / (image_height - 1);
+
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+            color pixel_color = ray_color(r);
 
             write_color(file, pixel_color);
         }
---
+++ src/ray.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.hpp"
+
+class ray {
+   public:
+    ray() {}
+    ray(const point3& origin, const vec3& dir) : orig(origin), dir(dir) {}
+
+    point3 origin() const { return orig; }
+    vec3 direction() const { return dir; }
+
+    point3 at(float t) const { return orig + t * dir; }
+
+   private:
+    point3 orig;
+    vec3 dir;
+};

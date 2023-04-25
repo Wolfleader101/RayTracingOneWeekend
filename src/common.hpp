55cc21b53434aace08e3a0c62b49eca326228631
Wolfleader <james.morgan@iinet.net.au>
2023-04-25 17:08:30 +0800

    working on final render

--- src/common.hpp
+++ src/common.hpp
@@ -11,7 +11,8 @@
 const double INFIN = std::numeric_limits<double>::infinity();
 const double PI = 3.1415926535897932385;
 
-inline float degreesToRadians(float degrees) { return degrees * PI / 180.0f; }
+inline float degreesToRadians(float degrees) { return degrees * (float)PI / 180.0f; }
+
 inline float randomFloat() {
     static std::uniform_real_distribution<float> distrubtion(0.0f, 1.0f);
     static std::mt19937 gen;
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 #include "Camera.hpp"
 #include "HittableList.hpp"
 #include "Material.hpp"
+#include "Sphere.hpp"
 #include "color.hpp"
 #include "common.hpp"
-#include "sphere.hpp"
 
-float hit_sphere(const point3& centre, float radius, const Ray& r) {
+float hit_Sphere(const point3& centre, float radius, const Ray& r) {
     vec3 oc = r.origin() - centre;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
@@ -43,9 +43,55 @@ color ray_color(const Ray& r, const Hittable& world, int depth) {
     return (1.0f - t) * color(1.0f, 1.0f, 1.0f) + t * color(0.5f, 0.7f, 1.0f);
 }
 
+HittableList random_scene() {
+    HittableList world;
+
+    auto ground_material = std::make_shared<Diffuse>(color(0.5, 0.5, 0.5));
+    world.Add(std::make_shared<Sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = randomFloat();
+            point3 center(a + 0.9f * randomFloat(), 0.2f, b + 0.9f * randomFloat());
+
+            if ((center - point3(4.f, 0.2f, 0.f)).length() > 0.9f) {
+                std::shared_ptr<Material> Sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    Sphere_material = std::make_shared<Diffuse>(albedo);
+                    world.Add(std::make_shared<Sphere>(center, 0.2, Sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = randomFloat(0, 0.5);
+                    Sphere_material = std::make_shared<Metal>(albedo, fuzz);
+                    world.Add(std::make_shared<Sphere>(center, 0.2, Sphere_material));
+                } else {
+                    // glass
+                    Sphere_material = std::make_shared<Dielectric>(1.5);
+                    world.Add(std::make_shared<Sphere>(center, 0.2, Sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = std::make_shared<Dielectric>(1.5);
+    world.Add(std::make_shared<Sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = std::make_shared<Diffuse>(color(0.4f, 0.2f, 0.1f));
+    world.Add(std::make_shared<Sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = std::make_shared<Metal>(color(0.7f, 0.6f, 0.5f), 0.0);
+    world.Add(std::make_shared<Sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
     // Image
-    const auto aspect_ratio = 16.0f / 9.0f;
+    const auto aspect_ratio = 3.f / 2.f;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 250;
@@ -55,27 +101,25 @@ int main() {
     std::stringstream ss;
 
     // World
+    // auto world = random_scene();
     HittableList world;
     auto material_ground = std::make_shared<Diffuse>(color(0.8f, 0.8f, 0.0f));
     auto material_center = std::make_shared<Diffuse>(color(0.7f, 0.3f, 0.3f));
-    // auto material_left = std::make_shared<Metal>(color(0.8f, 0.8f, 0.8f), 0.3f);
-    auto material_left = std::make_shared<Dielectric>(1.5f);
     auto material_right = std ::make_shared<Metal>(color(0.8f, 0.6f, 0.2f), 0.0f);
 
     world.Add(std ::make_shared<Sphere>(point3(0.0f, -100.5f, -1.0f), 100.0f, material_ground));
     world.Add(std ::make_shared<Sphere>(point3(0.0f, 0.0f, -1.0f), 0.5f, material_center));
-    world.Add(std ::make_shared<Sphere>(point3(-1.0f, 0.0f, -1.0f), 0.5f, material_left));
-    world.Add(std ::make_shared<Sphere>(point3(-1.0f, 0.0f, -1.0f), -0.4f, material_left));
+    world.Add(std ::make_shared<Sphere>(point3(-1.0f, 0.0f, -1.0f), -0.4f, material_center));
     world.Add(std ::make_shared<Sphere>(point3(1.0f, 0.0f, -1.0f), 0.5f, material_right));
 
     // Camera
-    point3 lookfrom(0, 1, 1);
+    point3 lookfrom(5, 2, 1);
     point3 lookat(0, 0, -1);
     vec3 vup(0, 1, 0);
     float dist_to_focus = 10.0;
-    float aperature = 0.1;
+    float aperature = 0.1f;
 
-    Camera cam(lookfrom, lookat, vup, 90, aspect_ratio, aperature, dist_to_focus);
+    Camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperature, dist_to_focus);
 
     // Render
     file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
